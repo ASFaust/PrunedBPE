e326066829adeaacf1ad7c18dee174ad0bcdb1fb
ASFaust <andreas.s.faust@gmail.com>
2023-10-09 19:19:54 +0200

    wip

--- src/BytePairEncoding.cpp
+++ src/BytePairEncoding.cpp
@@ -4,11 +4,32 @@
 #include <iostream>
 
 BPE::BPE(vector<vector<unsigned char>> production_rules) : production_rules(std::move(production_rules)) {
-    for (const auto& rule : this->production_rules) {
+    /*for (const auto& rule : this->production_rules) {
         string utf8_string(rule.begin(), rule.end());
         cout << "rule length: " << rule.size() << endl;
         cout << utf8_string << endl;
+    }*/
+    //production_rules now hold the production rules.
+    //sort the production rules by length, production_rules[0] being the longest
+    sort(this->production_rules.begin(), this->production_rules.end(),
+        [](const vector<unsigned char>& a, const vector<unsigned char>& b) {
+            return a.size() > b.size();
+        });
+    //that we just rearranged the tokens doesnt matter.
+}
+
+vector<int> BPE::encode(string inp){
+    vector<int> ret;
+    //now we need to use the production rules to encode the input
+    //we will use a greedy algorithm to do this
+    //we will start with the longest production rule and work our way down
+    vector<int> active_candidates; //indices into production_rules that are currently being considered
+    for (int i = 0; i < inp.size(); i++){
+        //production rules start at 0, but if they are emitted, they are added + 256
+        int current_token = inp[i];
+
     }
+    return ret;
 }
 
 void BPE::save(string filename) const {
